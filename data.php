e9803775f8543b8ab597557be11ae3ae3e5621db
axter <applestonedream@163.com>
2017-12-28 17:25:48 +0800

    同步我的内容

--- data.php
+++ data.php
@@ -1,9 +1,9 @@
 <?php
 
 $viewpass = '1234'; // 密码必须大于3位。留空则任何人可以访问
-$title = '标题';
-$subtitle = '子标题';
-$content = 'MarkDown';
+$title = '赵波的简历';
+$subtitle = 'Java开发工程师';
+$content = file_get_contents("me.md");;
 
 $data['local'] = 1;
 
